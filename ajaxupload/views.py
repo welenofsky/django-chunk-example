65aef1bb635a7422b158a4de00a9ba4339d8f9a5
Justin Welenofsky <welenofsky@gmail.com>
2015-02-02 12:39:34 -0800

    refactored to reduce line length

--- ajaxupload/views.py
+++ ajaxupload/views.py
@@ -6,7 +6,7 @@ import uuid
 from django.utils.timezone import now as timezone_now
 from django.core.files.storage import default_storage as storage
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 from django.views.decorators.csrf import csrf_exempt
 
 from .forms import MediaForm
@@ -19,77 +19,82 @@ def index(request):
 
 @csrf_exempt
 def upload(request):
-    if request.method == 'POST' and request.FILES['item']:
-        data = {}
-        data['files'] = []
-        if 'HTTP_CONTENT_DISPOSITION' in request.META:
-            """ Chunky """
-            f = request.FILES['item']
-            original_filename = re.findall(
-                'filename="(.*?)"',
-                request.META['HTTP_CONTENT_DISPOSITION'])[0]
-            # Chunk size regex
-            csr = re.compile(r'\d+')
-            cs_results = csr.findall(request.META['HTTP_CONTENT_RANGE'])
-            if len(cs_results) == 3:
-                ChunkInfo = {
-                    "FILENAME": original_filename,
-                    "CHUNK_START": cs_results[0],
-                    "CHUNK_END": cs_results[1],
-                    "CHUNK_TOTAL": cs_results[2],
-                }
-                if int(ChunkInfo["CHUNK_START"]) == 0:
-                    stuffed_form = MediaForm(request.POST, request.FILES)
-                    try:
-                        print("saving the first chunk\n")
-                        print("POST STUFF: %s" % request.POST['upload_id'])
-                        instance = stuffed_form.save()
-                        print("%s %s %s" % (
-                            instance.id,
-                            instance.item.name,
-                            instance.upload_id)
-                        )
-                    except Exception as e:
-                        print("broke that lol: %s" % e)
-                    print(original_filename)
-                    handle_uploaded_file(f, original_filename)
-                    print("\nCHUNKY\n")
-                    file = {
-                        "name": instance.item.name,
-                        "size": ChunkInfo["CHUNK_TOTAL"],
-                        "upload_id": instance.upload_id,
-                    }
-                    data['files'].append(file)
-                    print(json.dumps(data))
-                    print("%s\n%s\n%s\n" % (
-                        request.META['HTTP_CONTENT_DISPOSITION'],
-                        request.META['HTTP_CONTENT_RANGE'],
-                        request.META['CONTENT_TYPE'],
-                        )
-                    )
-                else:
-                    """ Handle sequential chunks """
-                    pass
-            else:
-                print('incorrect chunk sizes returned %s' % len(cs_results))
+    if request.method == 'GET' or 'item' not in request.FILES:
+        return HttpResponseBadRequest("Error: HTTP Status 400. Bad Request.")
+
+    data = {}
+    data['files'] = []
+
+    if 'HTTP_CONTENT_DISPOSITION' in request.META:
+        """ Chunky """
+        f = request.FILES['item']
+        original_filename = re.findall(
+            'filename="(.*?)"',
+            request.META['HTTP_CONTENT_DISPOSITION'])[0]
+        # Chunk size regex
+        csr = re.compile(r'\d+')
+        cs_results = csr.findall(request.META['HTTP_CONTENT_RANGE'])
+        if len(cs_results) == 3:
+            ChunkInfo = {
+                "FILENAME": original_filename,
+                "CHUNK_START": cs_results[0],
+                "CHUNK_END": cs_results[1],
+                "CHUNK_TOTAL": cs_results[2],
+            }
         else:
-            """ Not Chunky """
-            print(request.FILES['item'].name)
-            form = MediaForm(request.POST, request.FILES)
+            print('incorrect chunk sizes returned %s' % len(cs_results))
 
-            if form.is_valid():
-                instance = Media(item=request.FILES['item'])
-                instance.save()
-                file = {
-                    "name": instance.item.name,
-                    "size": request.META['CONTENT_LENGTH']
-                }
-                data['files'].append(file)
+        if ChunkInfo and int(ChunkInfo["CHUNK_START"]) == 0:
+            stuffed_form = MediaForm(request.POST, request.FILES)
+            try:
+                print("saving the first chunk\n")
+                print("POST STUFF: %s" % request.POST['upload_id'])
+                instance = stuffed_form.save()
+                print("%s %s %s" % (
+                    instance.id,
+                    instance.item.name,
+                    instance.upload_id)
+                )
+            except Exception as e:
+                print("broke that lol: %s" % e)
 
-        return HttpResponse(
-            json.dumps(data),
-            content_type="application/json"
-        )
+            print(original_filename)
+            handle_uploaded_file(f, original_filename)
+            print("\nCHUNKY\n")
+            file = {
+                "name": instance.item.name,
+                "size": ChunkInfo["CHUNK_TOTAL"],
+                "upload_id": instance.upload_id,
+            }
+            data['files'].append(file)
+            print(json.dumps(data))
+            print("%s\n%s\n%s\n" % (
+                request.META['HTTP_CONTENT_DISPOSITION'],
+                request.META['HTTP_CONTENT_RANGE'],
+                request.META['CONTENT_TYPE'],
+                )
+            )
+        else:
+            """ Handle sequential chunks """
+            pass
+    else:
+        """ Not Chunky """
+        print(request.FILES['item'].name)
+        form = MediaForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            instance = Media(item=request.FILES['item'])
+            instance.save()
+            file = {
+                "name": instance.item.name,
+                "size": request.META['CONTENT_LENGTH']
+            }
+            data['files'].append(file)
+
+    return HttpResponse(
+        json.dumps(data),
+        content_type="application/json"
+    )
 
 
 def get_upload_id(request):
